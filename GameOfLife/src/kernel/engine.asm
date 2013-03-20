17beffeb5f3d648b51c04fd9e92a737bc131871a
Piotr Dobrowolski <dobrypd@gmail.com>
2013-03-20 23:54:43 +0100

    second part of macros, different input

--- GameOfLife/src/kernel/engine.asm
+++ GameOfLife/src/kernel/engine.asm
@@ -17,45 +17,18 @@
 %define size_of_cell_t 2
 %define ptr_size 8
 
-
-;; get_cell_ptr
-;; writes &cells[i][j] to %1
-;; cells - %2
-;; i - %3
-;; j - %4
-;; width - %5
-;; using - %6
-%macro get_cell_ptr 6
-    mov %1, [%2 + %3 * %5 * ptr_size] ; cells[i] in %1
-    lea %6, [%4 * size_of_cell_t]
-    add %1, %6
-%endmacro
-
-;; with default location of variables
-%macro get_cell_ptr 1
-    get_cell_ptr rax, %1, r8, r9, rdi, r14
-%endmacro
-
-;; write_cell
+;; write_cell/3
 ;; writes proper destination[i][j]
-;; source - rdx
-;; destination - rcx
-;; arguments: %1 - rowoffset, %2 - collumn offset, %3 - number of neibours
-;; used registers: r13, r12, rax
+;; arguments:
+;;      %1 - &source[i][j],
+;;      %2 - &destination[i][j],
+;;      %3 - number of neibours
+;; used registers: rax
+;; registers after macro:
+;;      al  - source[i][j]
 %macro write_cell 3
-        ;; & source[i][j] = *(source + rowoffset) + collumnoffset
-        mov r13, rdx
-        add r13, %1
-        mov r13, [r13]
-        add r13, %2 ; r13 == & source[i][j]
-        mov r12, rcx
-        add r12, %1
-        mov r12, [r12]
-        add r12, %2 ; r12 == & destination[i][j]
-
-        mov r11, [r13]
-        mov al, byte [r13] ; al = source[i][j]
-        mov byte [r12], al ; destination[i][j] = source[i][j]
+        mov al, byte [%1] ; al = source[i][j]
+        mov byte [%2], al ; destination[i][j] = source[i][j]
 
         ;; check if alive
         test al, al ; source[i][j] == 0
@@ -68,7 +41,7 @@
         jg %%kill
         jump %%skip
     %%kill:
-        mov [r12], byte 0
+        mov [%2], byte 0
         jump %%skip
 
     %%dead:
@@ -76,8 +49,35 @@
         cmp %3, 3
         je %%skip
         ;; make alive
-        mov [r12], byte 1
+        mov [%2], byte 1
     %%skip:
+%endmacro
+
+;; find_and_write_cell/3
+;; writes proper destination[i][j]
+;; source - rdx
+;; destination - rcx
+;; arguments:
+;;      %1 - row offset,
+;;      %2 - collumn offset,
+;;      %3 - number of neibours
+;; used registers: r13, r12, rax
+;; registers after macro:
+;;      r13 - &source[i][j],
+;;      r12 - &destination[i][j]
+;;      al  - source[i][j]
+%macro find_and_write_cell 3
+        ;; & source[i][j] = *(source + rowoffset) + collumnoffset
+        mov r13, rdx
+        add r13, %1
+        mov r13, [r13]
+        add r13, %2 ; r13 == & source[i][j]
+        mov r12, rcx
+        add r12, %1
+        mov r12, [r12]
+        add r12, %2 ; r12 == & destination[i][j]
+
+        write_cell r13, r12, %3
 %endmacro
 
 ;; main function
