5398f5f4485e08f3d975cd9dedaf75caa7530c5b
Piotr Dobrowolski <dobrypd@gmail.com>
2013-05-19 20:59:20 +0200

    wrong binnary load

--- LinesDetection/src/kernel/image.c
+++ LinesDetection/src/kernel/image.c
@@ -4,6 +4,8 @@
  *
  */
 
+#define _GNU_SOURCE
+
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,8 +16,63 @@
 #include "image.h"
 
 const char * NETPBM_file_ext = "pgm";
+#define PGM_ASCII 2
+#define PGM_BIN 5
 
-static int load_ascii_pgm(image_t image, FILE* inputfile)
+
+static int scann_image_properties(image_t image, FILE * inputfile, char * * buf,
+        size_t * bufsize)
+{
+    int scanned_args;
+    int value;
+    int pgmtype;
+    debug_print(LVL_INFO, "Loading image properties from file %p.\n",
+            inputfile);
+    pgmtype = -1;
+
+    for (scanned_args = 0; scanned_args < 4;)
+    {
+        if (getline(buf, bufsize, inputfile) == -1)
+        {
+            return -1;
+        }
+
+        if ((*buf)[0] == '#')
+        {
+            debug_print(LVL_INFO, "comment:%s", (*buf));
+            continue;
+        }
+
+        if (scanned_args > 0)
+            sscanf(*buf, "%d\n", &value);
+        else
+            sscanf(*buf, "P%d\n", &value);
+
+        switch (scanned_args) {
+            case 0:
+                pgmtype = value;
+                break;
+            case 1:
+                image->width = value;
+                break;
+            case 2:
+                image->height = value;
+                break;
+            case 3:
+                image->max_deph = value;
+                break;
+        }
+
+        scanned_args++;
+    }
+    debug_print(LVL_INFO,
+            "PGM file info MAGIC:%d, WIDTH:%d, HEIGHT:%d, DEPH:%d.\n",
+                pgmtype, image->width, image->height, image->max_deph);
+
+    return pgmtype;
+}
+
+static int load_ascii_pgm(image_t image, FILE * inputfile)
 {
     unsigned int i, j;
     debug_print(LVL_INFO, "Scanning ascii file %p.\n", inputfile);
@@ -29,22 +86,38 @@ static int load_ascii_pgm(image_t image, FILE* inputfile)
     return 0;
 }
 
-static int load_binary_pgm(image_t image, FILE* inputfile)
+static int load_binary_pgm(image_t image, FILE * inputfile, char * * buf,
+        size_t * bufsize)
 {
-    unsigned int i, j;
+    int i, j;
+    size_t read;
     debug_print(LVL_INFO, "Scanning binnary file %p.\n", inputfile);
-    uint8_t loaded;
-    for (i = 0; i < image->height; ++i)
+    for (i = 0; i < image->height;)
     {
+        if ((read = getline(buf, bufsize, inputfile)) == -1)
+            return -1;
+
+        if ((*buf)[0] == '#')
+        {
+            debug_print(LVL_INFO, "comment:%s", (*buf));
+            continue;
+        }
+
+        if (read == 0)
+            continue;
+
+        if (read != image->width)
+        {
+            debug_print(LVL_INFO, "Row length %d\n", (int)read);
+            return -1;
+        }
+
         for (j = 0; j < image->width; ++j)
         {
-            if (fread(&loaded, sizeof(uint8_t), 1, inputfile) < 1)
-                return -1;
-            image->image_mono[i][j] = loaded;
+            image->image_mono[i][j] = (uint8_t)((*buf)[j]);
         }
-        if ((i < (image->height - 1))
-                && (fread(&loaded, sizeof(uint8_t), 1, inputfile) < 1))
-            return -1;
+
+        i++;
     }
     debug_print(LVL_INFO, "Scanned successfully file %p.\n", inputfile);
     return 0;
@@ -52,162 +125,89 @@ static int load_binary_pgm(image_t image, FILE* inputfile)
 
 image_t load_image_from_file(const char * filename)
 {
-    size_t scanned_bytes;
-    size_t number_bytes;
     char * buf = NULL;
-    char s_char;
-    int is_ascii; // otherwise binary
-    int scanned_args = 0;
+    size_t bufsize;
+    int pgm_type = -1;
     int i;
-
     debug_print(LVL_INFO, "Loading image from file %s.\n", filename);
 
-    buf = (char *)malloc(sizeof (char) * 1024);
+    // Alloc buffer and image structure).
     image_t image = (image_t)malloc(sizeof (struct _image_t));
-    if ((image == NULL) || (buf == NULL))
-    {
-        fputs("malloc error\n", stderr);
-        return NULL;
-    }
+    if (image == NULL)
+        goto MALLOC_ERR;
     image->type = NETPBM_PGM;
 
+    // Open file and scann image properties)
     FILE* inputfile = fopen(filename, "rb");
     if (inputfile == NULL)
     {
         fputs("cannot open input file\n", stderr);
-        free(image);
+        free_image(image);
         return NULL;
     }
-
-    // Check magic number.
-    debug_print(LVL_LOWER, "Scanning for MAGIC NUMBER %s.\n", filename);
-    scanned_bytes = fread(buf, sizeof (char), 2, inputfile);
-    if (scanned_bytes < 2)
-        goto FORMATERR;
-    buf[2] = '\0';
-    debug_print(LVL_LOWER, "Scanned:>%s\n", buf);
-    if (strcmp(buf, "P2") == 0)
-        is_ascii = 1;
-    else if (strcmp(buf, "P5") == 0)
-        is_ascii = 0;
-    else
-        goto FORMATERR;
-    debug_print(LVL_INFO, "Magic number is:%s\n", is_ascii ? ("P2") : ("P5"));
-
-    number_bytes = 0;
-
-    while (1)
+    if ((pgm_type = scann_image_properties(image, inputfile, &buf, &bufsize))
+            < 0)
+        goto FORMAT_ERR;
+    if (image->max_deph > 255)
     {
-        scanned_bytes = fread(&s_char, sizeof (char), 1, inputfile);
-        if (scanned_bytes == 0)
-            goto FORMATERR;
-        if (s_char == '#')
-        {
-            if (getline(&buf, &scanned_bytes, inputfile) == -1)
-                goto FORMATERR;
-            debug_print(LVL_INFO, "Scanned comment:%s\n", buf);
-            continue;
-        }
-        else
-        {
-            if (isspace(s_char))
-            {
-                if (number_bytes > 0)
-                {
-                    buf[number_bytes] = '\0';
-                    if (scanned_args == 0)
-                    {
-                        sscanf(buf, "%d", &(image->width));
-                        scanned_args = 1;
-                        debug_print(LVL_LOWER, "Scanned width:%d\n",
-                                image->width);
-                        number_bytes = 0;
-                    }
-                    else if (scanned_args == 1)
-                    {
-                        sscanf(buf, "%d", &(image->height));
-                        scanned_args = 2;
-                        debug_print(LVL_LOWER, "Scanned height:%d\n",
-                                    image->height);
-                        number_bytes = 0;
-                    }
-                    else if (scanned_args == 2)
-                    {
-                        sscanf(buf, "%d", &(image->max_deph));
-                        scanned_args = 3;
-                        debug_print(LVL_LOWER, "Scanned deph:%d\n",
-                                    image->max_deph);
-                        number_bytes = 0;
-                    }
-                }
-                else
-                {
-                    number_bytes = 0;
-                }
-            }
-            else
-            {
-                if (scanned_args == 3)
-                {
-                    fseek(inputfile, -1, SEEK_CUR);
-                    break;
-                }
-                buf[number_bytes++] = s_char;
-            }
-        }
-
+        fputs("in this version cannot load images with deph > 255\n", stderr);
+        goto FORMAT_ERR;
     }
 
+    // Alloc raw image.
     image->image_mono = (raw_image_mono_8_t)malloc(sizeof(uint8_t *) * image->height);
     if (image->image_mono == NULL)
-    {
-        free(buf);
-        fclose(inputfile);
-        free_image(image);
-        fputs("malloc error\n", stderr);
-        return NULL;
-    }
+        goto MALLOC_ERR;
     for (i = 0; i < image->height; ++i)
     {
         image->image_mono[i] = (uint8_t *)malloc(sizeof(uint8_t) * image->width);
         if (image->image_mono[i] == NULL)
-        {
-            free(buf);
-            fclose(inputfile);
-            free_image(image);
-            fputs("malloc error\n", stderr);
-            return NULL;
-        }
+            goto MALLOC_ERR;
     }
 
-    if (is_ascii)
+    debug_print(LVL_INFO, "Next line %p %p %p\n", image, image->image_mono, image->image_mono[0]);
+    // Load pixels. (2 types of pgm file, binary or ascii).
+    if (pgm_type == PGM_ASCII)
     {
         if (load_ascii_pgm(image, inputfile) != 0)
-            goto FORMATERR;
+            goto FORMAT_ERR;
+    }
+    else if (pgm_type == PGM_BIN)
+    {
+        if (load_binary_pgm(image, inputfile, &buf, &bufsize) != 0)
+            goto FORMAT_ERR;
+
     }
     else
     {
-        if (load_binary_pgm(image, inputfile) != 0)
-            goto FORMATERR;
-
+        fputs("pgm magic number not recognized, only P2 or P5\n", stderr);
+        goto FORMAT_ERR;
     }
 
-    free(buf);
+    if (buf != NULL)
+        free(buf);
     fclose(inputfile);
     return image;
 
-FORMATERR:
-    debug_print(LVL_WARNING, "Input error, bad format %s.\n", filename);
-    free(buf);
+FORMAT_ERR:
+    if (buf != NULL)
+        free(buf);
     fclose(inputfile);
     free_image(image);
     fputs("wrong input format, only pgm\n", stderr);
+    return NULL;
+MALLOC_ERR:
+    if (buf != NULL)
+        free(buf);
+    fclose(inputfile);
+    free_image(image);
+    fputs("malloc error\n", stderr);
     return NULL;
 }
 
 image_t copy_image(image_t image)
 {
-    int i;
+    int i, j;
     image_t new_image = (image_t)malloc(sizeof (struct _image_t));
     new_image->width = image->width;
     new_image->height = image->height;
@@ -221,6 +221,7 @@ image_t copy_image(image_t image)
         fputs("malloc error\n", stderr);
         return NULL;
     }
+
     for (i = 0; i < image->height; ++i)
     {
         new_image->image_mono[i] = (uint8_t *) malloc(
@@ -231,7 +232,13 @@ image_t copy_image(image_t image)
             fputs("malloc error\n", stderr);
             return NULL ;
         }
+
+        for (j = 0; j < image->width; ++j)
+        {
+            new_image->image_mono[i][j] = image->image_mono[i][j];
+        }
     }
+
     return new_image;
 }
 
@@ -260,8 +267,10 @@ void save_image_to_file(image_t image, const char * filename)
     fprintf(output, "P5\n%d %d\n%d\n", image->width, image->height,
             image->max_deph);
     for (i = 0; i < image->height; ++i)
+    {
         fwrite(image->image_mono[i], sizeof(uint8_t), image->width, output);
         fputc((int)'\n', output);
+    }
 }
 
 void free_image(image_t image)
@@ -276,10 +285,13 @@ void free_image(image_t image)
                 if (image->image_mono[i] != NULL)
                 {
                     free(image->image_mono[i]);
+                    image->image_mono[i] = NULL;
                 }
             }
             free(image->image_mono);
+            image->image_mono = NULL;
         }
         free(image);
+        image = NULL;
     }
 }
